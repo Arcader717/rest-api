bf33b515693786442b46a9aabc45e4acd592c49e
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-09-29 17:17:44 +0000

    Updated main.py
    Modified to include the hypercorn hosting package. May need to make a new branch for testing in github

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from quart import Quart, render_template
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+import asyncio
 
 
+config = Config()
 app = Quart(__name__)
+config.bind = "0.0.0.0:3750"
 
 @app.route("/")
 async def index():
   return await render_template("index.html")
+
+
+asyncio.run(serve(app, config))
