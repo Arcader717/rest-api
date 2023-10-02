fd6a8862d77923f1c045c68db6e00075ad0954b7
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-10-02 02:23:58 +0000

    Update main.py
    Had to fix something I forgot to commit

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from quart import Quart, render_template
+import asyncio
+import json
+
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
-import asyncio
+from flask import Flask, render_template, jsonify, request
+
+pingMsg = "Pong!" # the response to the "/ping" http request
 
 
+apiDict = { # A dict containing the avaliable api links
+  'ping': {
+    'url': '/ping',
+    'method': 'GET'
+  },
+  'Change Ping Response': {
+    'url': '/ping/post'
+  }
+}
 config = Config()
-app = Quart(__name__)
+app = Flask(__name__)
 config.bind = "0.0.0.0:3750"
 
 @app.route("/")
-async def index():
-  return await render_template("index.html")
+def index():
+  return render_template("index.html")
 
 @app.get("/ping")
-async def pong():
-  return await render_template("")
+def pong():
+  return render_template("")
 
 asyncio.run(serve(app, config))
